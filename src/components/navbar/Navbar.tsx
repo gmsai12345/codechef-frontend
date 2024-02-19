a63c5876be4f765658c5ea3f62b46daf3a5e9e8e
gmsai12345 <saidhakshan4@gmail.com>
2024-02-20 02:44:35 +0530

    first commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,9 +7,11 @@ const TopNavBar: React.FC = () => {
     return (
         <Menu mode="horizontal" >
             <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">About</Menu.Item>
-            <Menu.Item key="3">Contact</Menu.Item>
-            <Menu.Item key="4" >
+            <Menu.Item key="2">Doctors</Menu.Item>
+            <Menu.Item key="3">Hospitals</Menu.Item>
+            <Menu.Item key="4">Diagnostics</Menu.Item>
+            <Menu.Item key="5">TeleMedicines & Appoinment</Menu.Item>
+            <Menu.Item key="6" >
                 <SigninButton />
             </Menu.Item>
         </Menu>
