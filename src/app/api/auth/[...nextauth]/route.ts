e1ee2c19a2c96cc6c5d8e031847e960ae6b31b40
gmsai12345 <saidhakshan4@gmail.com>
2024-02-20 04:43:59 +0530

    init commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,8 +7,8 @@ const handler = NextAuth({
       clientId: "317177919758-9c48dg5rqnsnfbr36ud6ohm7vsutc99s.apps.googleusercontent.com",
       clientSecret: "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",
     }),
-    secret:process.env.SECRET
   ],
+  secret:process.env.SECRET
 });
 
 export { handler as GET, handler as POST };
