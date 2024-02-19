6ab23258a9b0ae2ec3056305c7118b63bda0b90a
gmsai12345 <saidhakshan4@gmail.com>
2024-02-19 19:54:03 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import { Button } from "antd";
-import Image from "next/image";
-
+import Image from 'next/image'
+import Navbar from "./components/Navbar"
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-      />
-          <Button type="primary">Button</Button>
-
-    </div>
-  );
+   <Navbar/>
+  )
 }
